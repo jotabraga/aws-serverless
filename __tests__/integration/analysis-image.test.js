a32da5e2d32f0ee88391f9ba7bdfc982e5bad71b
jotabraga <jpmbraga.feg@gmail.com>
2023-02-01 19:59:01 -0300

    feat: create image analysis test suite

---
+++ __tests__/integration/analysis-image.test.js
@@ -0,0 +1,7 @@
+describe("Image analyse cloud function test suite", () => {
+  test.todo(
+    "It should return the main certainties about a dog image for a valid dog picture"
+  );
+  test.todo("It should return statusCode 400 for undefined picture url");
+  test.todo("It should return statusCode 500 for invalid picture url");
+});
---
+++ annotation.js
@@ -0,0 +1,11 @@
+/*
+npm i -g ntl => type 'ntl' in terminal to get script run options and 'nt' executes last run
+npm jest --init => 
+    ✔ Would you like to use Jest when running "test" script in "package.json"? … yes
+    ✔ Would you like to use Typescript for the configuration file? … no
+    ✔ Choose the test environment that will be used for testing › node
+    ✔ Do you want Jest to add coverage reports? … yes
+    ✔ Which provider should be used to instrument code for coverage? › v8
+    ✔ Automatically clear mock calls, instances, contexts and results before every test? … yes
+    => creates jest.config.js
+*/
